f974e9be86f7fd1d1d00984a1658410b9c2f8ffa
uldcra <uldcra@gmail.com>
2018-12-24 00:28:19 +0100

    Crendo archivo excel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+/**
+ * rutas
+ */
+import { AppRoutingModule } from 'src/app/app.routes';
 
 
-
+ /**
+  * Componentes
+  */
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/menu/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -22,6 +29,8 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/formularios/formularios.component.ts
+++ src/app/components/formularios/formularios.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { ExcelService } from 'src/app/services/excel.service';
+
 
 @Component({
   selector: 'app-formularios',
@@ -7,9 +11,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormulariosComponent implements OnInit {
 
-  constructor() { }
+  public product=[{
+    fecha:'',
+    producto:'',
+    Trazabilidad:'',
+    plage:'',
+    sabor:'',
+  }];
+
+  materialForm=new FormGroup({
+    fecha:new FormControl('',Validators.required),
+    producto:new FormControl('',[Validators.required,Validators.minLength(4)]),
+    Trazabilidad:new FormControl('',Validators.required),
+    plage:new FormControl('',Validators.required),
+    sabor:new FormControl('',Validators.required),
+  });
+  constructor(private excelService:ExcelService) { 
+    
+  }
 
   ngOnInit() {
   }
-
+  onSubmit(){
+    console.log(this.materialForm);
+    this.product=[{
+      fecha:this.materialForm.controls.fecha.value,
+      producto:this.materialForm.controls.producto.value,
+      Trazabilidad:this.materialForm.controls.Trazabilidad.value,
+      plage:this.materialForm.controls.plage.value,
+      sabor:this.materialForm.controls.sabor.value,
+      
+    }]
+    this.excelService.exportAsExcelFile(this.product, 'sample');
+  }
+  exportAsXLSX():void {
+    this.excelService.exportAsExcelFile(null, 'sample');
+ }
 }
