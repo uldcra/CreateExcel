2e23947a190b45ff0f29a2e80f729176318071fd
uldcra <uldcra@gmail.com>
2018-12-25 10:45:01 +0100

    Formulario de material

--- src/app/components/formularios/formularios.component.ts
+++ src/app/components/formularios/formularios.component.ts
@@ -17,17 +17,31 @@ export class FormulariosComponent implements OnInit {
     Trazabilidad:'',
     plage:'',
     sabor:'',
+    textura:'',
+    color:'',
+    manchas:'',
+    transporte:'',
+    olor:'',
+    pudedumbre:'',
+    depCalidad:''
   }];
 
   materialForm=new FormGroup({
-    fecha:new FormControl('',Validators.required),
+    fecha:new FormControl(this.fecha(),Validators.required),
     producto:new FormControl('',[Validators.required,Validators.minLength(4)]),
     Trazabilidad:new FormControl('',Validators.required),
     plage:new FormControl('',Validators.required),
     sabor:new FormControl('',Validators.required),
+    textura:new FormControl('v'),
+    color:new FormControl('v'),
+    manchas:new FormControl('v'),
+    transporte:new FormControl('v'),
+    olor:new FormControl('v'),
+    pudedumbre:new FormControl('v'),
+    depCalidad:new FormControl('v'),
   });
   constructor(private excelService:ExcelService) { 
-    
+   
   }
 
   ngOnInit() {
@@ -40,11 +54,38 @@ export class FormulariosComponent implements OnInit {
       Trazabilidad:this.materialForm.controls.Trazabilidad.value,
       plage:this.materialForm.controls.plage.value,
       sabor:this.materialForm.controls.sabor.value,
+      textura:this.materialForm.controls.textura.value,
+      color:this.materialForm.controls.color.value,
+      manchas:this.materialForm.controls.manchas.value,
+      transporte:this.materialForm.controls.transporte.value,
+      olor:this.materialForm.controls.olor.value,
+      pudedumbre:this.materialForm.controls.pudedumbre.value,
+      depCalidad:this.materialForm.controls.depCalidad.value,
       
-    }]
+    }];
     this.excelService.exportAsExcelFile(this.product, 'sample');
   }
   exportAsXLSX():void {
     this.excelService.exportAsExcelFile(null, 'sample');
+ }
+
+ fecha(){
+  let today = new Date();
+  let dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+  
+  /*if(dd<10) {
+      dd = '0'+dd
+  } 
+  
+  if(mm<10) {
+      mm = '0'+mm
+  } */
+  
+  let myfecha = mm + '/' + dd + '/' + yyyy;
+  myfecha=yyyy+'-'+mm + '-' + dd ;
+  console.log(myfecha);
+  return myfecha;
  }
 }
